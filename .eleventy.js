28d3f469d0c7a2637ff2b467743187835a6e7365
dogwonder <dogwonder@gmail.com>
2022-11-18 13:37:31 +0000

    upgrade layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,69 @@
-const prettyBytes = require("pretty-bytes");
+const byteSize = require("byte-size");
 const shortHash = require("short-hash");
 const lodash = require("lodash");
+const getObjectKey = require("./utils/getObjectKey.js");
+const calc = require("./utils/calc.js");
+
+function hasUrl(urls, requestedUrl) {
+	// urls comes from sites[vertical].urls, all requestedUrls (may not include trailing slash)
+
+	// TODO lowercase just the origins
+	let lowercaseUrls = urls.map(url => url.toLowerCase());
+
+	if(requestedUrl && typeof requestedUrl === "string") {
+		// TODO lowercase just the origins
+		requestedUrl = requestedUrl.toLowerCase();
+		if(lowercaseUrls.indexOf(requestedUrl) > -1 || requestedUrl.endsWith("/") && lowercaseUrls.indexOf(requestedUrl.substr(0, requestedUrl.length - 1)) > -1) {
+			return true;
+		}
+	}
+
+	return false;
+}
 
 function showDigits(num, digits = 2) {
-	return parseFloat(num).toFixed(digits);
+	let toNum = parseFloat(num);
+	let afterFixed = toNum.toFixed(digits);
+	return afterFixed;
+}
+
+function pad(num) {
+	return (num < 10 ? "0" : "") + num;
+}
+
+function mapProp(prop, targetObj) {
+	if(Array.isArray(prop)) {
+		let otherprops = [];
+		prop =  prop.map(entry => {
+			// TODO this only works as the first entry
+			if(entry === ":newest") {
+				entry = Object.keys(targetObj).sort().pop();
+			} else if(entry.indexOf("||") > -1) {
+				for(let key of entry.split("||")) {
+					if(lodash.get(targetObj, [...otherprops, key])) {
+						entry = key;
+						break;
+					}
+				}
+			}
+			otherprops.push(entry);
+
+			return entry;
+		});
+	}
+
+	return prop;
+}
+
+function getLighthouseTotal(entry) {
+	return entry.lighthouse.performance * 100 +
+		entry.lighthouse.accessibility * 100 +
+		entry.lighthouse.bestPractices * 100 +
+		entry.lighthouse.seo * 100;
 }
 
 module.exports = function(eleventyConfig) {
-	eleventyConfig.addFilter("shortHash", function(value) {
-		return shortHash(value);
-	});
+	eleventyConfig.addFilter("shortHash", shortHash);
 
 	eleventyConfig.addFilter("repeat", function(str, times) {
 		let result = '';
@@ -21,13 +75,7 @@ module.exports = function(eleventyConfig) {
 		return result;
 	});
 
-	eleventyConfig.addFilter("head", function(arr, num) {
-		if(num) {
-			return arr.slice(0, num);
-		}
-		return arr;
-	});
-
+	// first ${num} entries (and the last entry too)
 	eleventyConfig.addFilter("headAndLast", function(arr, num) {
 		if(num && num < arr.length) {
 			let newArr = arr.slice(0, num);
@@ -37,51 +85,63 @@ module.exports = function(eleventyConfig) {
 		return arr;
 	});
 
-	eleventyConfig.addFilter("displayUrl", function(url) {
-		url = url.replace("https://www.", "");
+	eleventyConfig.addFilter("displayUrl", function(url, keepWww = false) {
+		if(!keepWww) {
+			url = url.replace("https://www.", "");
+		}
 		url = url.replace("https://", "");
+		if(url.endsWith("/index.html")) {
+			url = url.replace("/index.html", "/");
+		}
 		return url;
+	});
+
+	eleventyConfig.addFilter("showDigits", function(num, digits) {
+		return showDigits(num, digits);
 	});
 
 	eleventyConfig.addFilter("displayTime", function(time) {
 		let num = parseFloat(time);
-		if(num > 1000) {
+		if(num > 850) {
 			return `${showDigits(num / 1000, 2)}s`;
 		}
 		return `${showDigits(num, 0)}ms`;
 	});
 
 	eleventyConfig.addFilter("displayFilesize", function(size) {
-		return prettyBytes(size);
+		let normalizedSize = byteSize(size, { units: 'iec', precision: 0 });
+		let unit = normalizedSize.unit;
+		let value = normalizedSize.value;
+		return `<span class="filesize">${value}<span class="filesize-label-sm">${unit.substr(0,1)}</span><span class="filesize-label-lg"> ${unit}</span></span>`;
 	});
 
-	function pad(num) {
-		return (num < 10 ? "0" : "") + num;
-	}
 	eleventyConfig.addFilter("displayDate", function(timestamp) {
 		let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 		let date = new Date(timestamp);
 		let day = `${months[date.getMonth()]} ${pad(date.getDate())}`;
-		return `${day} ${pad(date.getHours())}:${pad(date.getMinutes())}`;
+		return `${day} <span class="leaderboard-hide-md">${pad(date.getHours())}:${pad(date.getMinutes())}</span>`;
 	});
 
-	function mapProp(prop, targetObj) {
-		if(Array.isArray(prop)) {
-			prop =  prop.map(entry => {
-				if(entry === ":lastkey") {
-					let ret;
-					for(let key in targetObj) {
-						ret = key;
-					}
-					return ret;
-				}
+	eleventyConfig.addFilter("sortCumulativeScore", (obj) => {
+		return obj.sort((a, b) => {
 
-				return entry;
-			});
-		}
+			let newestKeyA = Object.keys(a).sort().pop();
+			let newestKeyB = Object.keys(b).sort().pop();
 
-		return prop;
-	}
+			// Lighthouse error
+			// e.g. { url: 'https://mangoweb.net/', error: 'Unknown error.' }
+			if(b[newestKeyB].error && a[newestKeyA].error) {
+				return 0;
+			} else if(b[newestKeyB].error) {
+				return -1;
+			} else if(a[newestKeyA].error) {
+				return 1;
+			}
+
+			// lower is better
+			return a[newestKeyA].ranks.cumulative - b[newestKeyB].ranks.cumulative;
+		});
+	});
 
 	// Works with arrays too
 	// Sort an object that has `order` props in values.
@@ -126,38 +186,35 @@ module.exports = function(eleventyConfig) {
 		return sorted;
 	});
 
-	eleventyConfig.addFilter("getObjectKey", (obj, which = ":first") => {
-		let ret;
-		let newestTimestamp = 0;
-		for(let key in obj) {
-			ret = key;
-			if(which === ":newest") {
-				if(obj[key].timestamp > newestTimestamp) {
-					newestTimestamp = obj[key].timestamp;
-					ret = key;
-				}
-			}
-			if(which === ":first") {
-				return ret;
-			}
-		}
-		return ret;
-	});
+	eleventyConfig.addFilter("getObjectKey", getObjectKey);
 
-	eleventyConfig.addFilter("filterToUrls", (obj, urls = []) => {
+	function filterResultsToUrls(obj, urls = [], skipKeys = []) {
 		let arr = [];
 		for(let key in obj) {
-			let result;
-			for(let filename in obj[key]) {
-				result = obj[key][filename];
-				break;
+			if(skipKeys.indexOf(key) > -1) {
+				continue;
 			}
-			if(urls.indexOf(result.requestedUrl) > -1) {
+
+			let result;
+			let newestFilename = Object.keys(obj[key]).sort().pop();
+			result = obj[key][newestFilename];
+			// urls comes from sites[vertical].urls, all requestedUrls (may not include trailing slash)
+			if(urls === true || result && hasUrl(urls, result.requestedUrl)) {
 				arr.push(obj[key]);
 			}
 		}
 		return arr;
+	}
+
+	eleventyConfig.addFilter("getSites", (results, sites, vertical, skipKeys = []) => {
+		let urls = sites[vertical].urls;
+		let isIsolated = sites[vertical].options && sites[vertical].options.isolated === true;
+		let prunedResults = isIsolated ? results[vertical] : results;
+		return filterResultsToUrls(prunedResults, urls, skipKeys);
 	});
+
+	// Deprecated, use `getSites` instead, it works with isolated categories
+	eleventyConfig.addFilter("filterToUrls", filterResultsToUrls);
 
 	eleventyConfig.addFilter("hundoCount", (entry) => {
 		let count = 0;
@@ -177,19 +234,95 @@ module.exports = function(eleventyConfig) {
 		return count;
 	});
 
-	eleventyConfig.addFilter("lighthouseTotal", (entry) => {
-		let total = 0;
-		total += entry.lighthouse.performance;
-		total += entry.lighthouse.accessibility;
-		total += entry.lighthouse.bestPractices;
-		total += entry.lighthouse.seo;
-		return Math.round(total * 100);
+	eleventyConfig.addFilter("notGreenCircleCount", (entry) => {
+		let count = 0;
+		if(entry.lighthouse.performance < .9) {
+			count++;
+		}
+		if(entry.lighthouse.accessibility < .9) {
+			count++;
+		}
+		if(entry.lighthouse.bestPractices < .9) {
+			count++;
+		}
+		if(entry.lighthouse.seo < .9) {
+			count++;
+		}
+
+		return count;
 	});
 
-	eleventyConfig.addPassthroughCopy({
-		"./node_modules/chartist/dist/chartist.css": "chartist.css",
-		"./node_modules/chartist/dist/chartist.js": "chartist.js",
+	eleventyConfig.addFilter("hundoCountTotals", (counts, entry) => {
+		if(!entry.error && !isNaN(entry.lighthouse.performance)) {
+			counts.total++;
+		}
+
+		if(entry.lighthouse.performance === 1) {
+			counts.performance++;
+		}
+		if(entry.lighthouse.accessibility === 1) {
+			counts.accessibility++;
+		}
+		if(entry.lighthouse.bestPractices === 1) {
+			counts.bestPractices++;
+		}
+		if(entry.lighthouse.seo === 1) {
+			counts.seo++;
+		}
+
+		if(entry.lighthouse.performance === 1 && entry.lighthouse.accessibility === 1 && entry.lighthouse.bestPractices === 1 && entry.lighthouse.seo === 1) {
+			counts.perfect++;
+		}
+
+		return counts;
 	});
-	eleventyConfig.addPassthroughCopy("chart.js");
-	eleventyConfig.addTemplateFormats("css");
+
+	eleventyConfig.addFilter("lighthouseTotal", getLighthouseTotal);
+
+	eleventyConfig.addFilter("addLighthouseTotals", (arr) => {
+		/* special case */
+		for(let obj of arr) {
+			for(let entry in obj) {
+				if(obj[entry].lighthouse) {
+					obj[entry].lighthouse[":lhtotal"] = getLighthouseTotal(obj[entry]);
+				}
+			}
+		}
+		return arr;
+	});
+
+	eleventyConfig.addFilter("toJSON", function(obj) {
+		return JSON.stringify(obj);
+	});
+
+	eleventyConfig.addFilter("calc", calc);
+
+	function getWeeklyServiceCacheBuster() {
+		let d = new Date();
+		// Weekly
+		return `_${d.getFullYear()}${pad(d.getMonth()+1)}_${d.getDate() % 7}`;
+	}
+	eleventyConfig.addFilter("generatorImageUrl", (url) => {
+		return `https://v1.generator.11ty.dev/image/${encodeURIComponent(url)}/${getWeeklyServiceCacheBuster()}/`;
+	});
+
+	eleventyConfig.addPairedShortcode("starterMessage", (htmlContent) => {
+		if(process.env.SITE_NAME !== "speedlify") {
+			return htmlContent;
+		}
+		return "";
+	});
+
+	// Assets
+	eleventyConfig.addPassthroughCopy({
+		"./node_modules/chartist/dist/chartist.js": "chartist.js",
+		"./node_modules/chartist/dist/chartist.css.map": "chartist.css.map",
+	});
+
+	eleventyConfig.addWatchTarget("./assets/");
+
+	eleventyConfig.setBrowserSyncConfig({
+		ui: false,
+		ghostMode: false
+	});
 };
